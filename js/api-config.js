1d27337050021636ad244dbf1a005d306c983010
Akio Zaki Salomon <azsalomon69@gmail.com>
2026-08-26 01:27:07 +0800

    Connect frontend to Render API

--- js/api-config.js
+++ js/api-config.js
@@ -1,2 +1,2 @@
-// Set this to your Render API URL after deployment. It contains no secret.
-window.ACE_API_URL = window.ACE_API_URL || 'http://localhost:3000';
+// Public Render API address. This contains no secret.
+window.ACE_API_URL = window.ACE_API_URL || 'https://ace-clockinandout.onrender.com';
